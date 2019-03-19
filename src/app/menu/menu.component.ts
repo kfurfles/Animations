583f40efb9126d00ec863bc4365e93f25e4f32a9
Kelvin Silva Visitacao <ksilvavi@everis.com>
2019-03-18 22:18:44 -0300

    generic cli message

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { r as routes } from './../app-routing.module';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: '.app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MenuComponent implements OnInit {
 
