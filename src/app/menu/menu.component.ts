ac93afb62c698447ee15c414d6aad2eea7c2b09a
Kelvin Silva Visitacao <ksilvavi@everis.com>
2019-03-18 21:18:22 -0300

    generic cli message

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  routes: Array<any> = routes;
+  routes: Array<any> = routes.filter(r => r.show);
 
   ngOnInit() {
   }
